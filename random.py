daebcf15e5173f91c06e27b663ca7e4313588836
Failed <71407166+FailedInCoding@users.noreply.github.com>
2021-03-30 14:59:29 +0200

    Add files via upload

---
+++ random.py
@@ -0,0 +1,11 @@
+ def find_shortest_path(graph, start, end):
+        dist = {start: [start]}
+        q = deque(start)
+        while len(q):
+            at = q.popleft()
+            for next in graph[at]:
+                if next not in dist:
+                    dist[next] = [dist[at], next]
+                    q.append(next)
+        return dist.get(end)
+        
